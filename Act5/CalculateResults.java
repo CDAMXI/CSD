8422a0f7afe02aec9e4cb65215720862983042ec
CDAMXI <cdachongm@gmail.com>
2025-02-12 12:06:47 +0100

    New

---
+++ Act5/CalculateResults.java
@@ -0,0 +1,14 @@
+public class CalculateResults extends Thread{
+    private String result = "Not Calculated";
+
+    public void run(){result = calculate();}
+
+    private String calculate(){
+        try{Thread.sleep(10000);}
+        catch(InterruptedException e){}
+        System.out.println("Agent thread finishes its calculation");
+        return "Calculation done";
+    }
+
+    public String getResults(){return result;}
+}
---
+++ Act5/Example_1.java
@@ -0,0 +1,8 @@
+public class Example_1 {
+    public static void main(String[] args) {
+        CalculateResults cr = new CalculateResults();
+        cr.start();
+        System.out.println("Main thread in execution");
+        System.out.println("Main thread gets the result: " + cr.getResults());
+    }
+}
