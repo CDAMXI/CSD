5a618b861fa135fdff34fc40010f4dcc5f29f42f
CDAMXI <cdachongm@gmail.com>
2025-02-19 12:54:31 +0100

    Añadir clases BoundedBuffer y ProducerConsumer para implementar un sistema de productor-consumidor

---
+++ Act4_Tema3/ProducerConsumer.java
@@ -0,0 +1,21 @@
+public class ProducerConsumer {
+    static BoundedBuffer buf = new BoundedBuffer(1);
+
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    buf.put(i);
+                }
+            }
+        }, "producer").start();
+
+        new Thread(new Runnable() {
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println(buf.get());
+                }
+            }
+        }, "consumer").start();
+    }
+}
